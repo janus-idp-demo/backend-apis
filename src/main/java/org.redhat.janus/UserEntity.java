aadfa356ded2c534dc0a8f25382ffb71919d0c6d
Jason Froehlich <38667521+jayfray12@users.noreply.github.com>
2023-07-18 12:54:57 -0400

    Create UserEntity.java

---
+++ src/main/java/org.redhat.janus/UserEntity.java
@@ -0,0 +1,15 @@
+package org.redhat.janus;
+
+public class UserEntity {
+  public String name;
+  public String email;
+  public String id;
+
+  public UserEntity() {
+  }
+
+  public UserEntity(String name, String email) {
+    this.name = name;
+    this.email = eamil;
+  }
+}
